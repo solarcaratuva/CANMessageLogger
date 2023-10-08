c5b9726557f9cfdbe331188ee72fd63b70bcf93d
colbycheese55 <112818133+colbycheese55@users.noreply.github.com>
2023-10-08 15:16:58 -0400

    ready for testing

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import time
+import graphs
+import test
+import threading
 
 
 logFilePath = "Logging_Data.txt"
+logFilePath = "test.txt"
 linesReadCount = 0
+running = True
 
-tractedValues = dict()
+tractedValues = {"throttle": dict()}
+currentGraphs = dict()
 
 def checkForObviousErrors(key): #TODO
     return False
 
 def messageHandler(unparsedMessage):
-    messageDict, timestamp = dict() # get from Jimmy's parser
+    messageDict, timestamp = test.testing() # get from Jimmy's parser
     for key in messageDict:
         if key in tractedValues:
             tractedValues[key][timestamp] = messageDict[key]
@@ -22,18 +28,54 @@ def messageHandler(unparsedMessage):
             
 
 def dataUpdater():
-    global linesReadCount
-    logData = open(logFilePath, "r").readlines()
-    newLogData = logData[linesReadCount:]
-    linesReadCount = len(logData)
+    global linesReadCount, currentGraphs
+    while running == True:
+        logData = open(logFilePath, "r").readlines()
+        newLogData = logData[linesReadCount:]
+        linesReadCount = len(logData)
 
-    for item in newLogData:
-        print(item)
-        #messageHandler(item)
+        for item in newLogData:
+            messageHandler(item)
+        #print("UPDATED")
 
-if __name__ == "__main__": #testing
-    while True:
-        dataUpdater()
-        print("UPDATED")
-        time.sleep(5)
+        for graph in currentGraphs:
+            graph.update()
+        time.sleep(1)
+
+
+
+def userInput():
+    global tractedValues, currentGraphs, running
+    while running == True:
+        command = input("Enter a command: ")
+        command = command.split()   # split by spaces; [0] = command, [1] = item name, [2...] = other arguments
+
+        if command[0] == "quit":
+            running = False
+            continue
+        if len(command) <= 1:
+            print("COMMAND MISSING ARGUMENTS")
+            continue
+        if command[1] not in tractedValues:
+            print(f"THE VALUE \"{command[1]}\" IS NOT A TRACKED VALUE")
+            continue
+        match command[0]:
+            case "add":
+                thisDict = tractedValues[command[1]]
+                thisGraph = graphs.Graph(thisDict)
+                currentGraphs[command[1]] = thisGraph
+            case "rm":
+                thisGraph = currentGraphs[command[1]]
+                thisGraph.delete()
+                del currentGraphs[command[1]]
+            case _:
+                print("UNKNOWN COMMAND ENTERED")
+
+
+if __name__ == "__main__": #Main Method
+    dataUpdaterThread = threading.Thread(target=dataUpdater)
+    dataUpdaterThread.start()
+    userInputThread = threading.Thread(target=userInput)
+    userInputThread.start()
+
         
