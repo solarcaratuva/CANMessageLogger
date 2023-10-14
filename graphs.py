0061f592c4585d6a2d1952ba754bba2a05b01c80
AlexTalreja <alextalreja@gmail.com>
2023-10-14 15:07:46 -0400

    update and delete method

--- graphs.py
+++ graphs.py
@@ -2,8 +2,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 class Graph:
-    def __init__(self, dict, name="", start=-1, end=-1):
+    
+    startIndex = 0
+    endIndex = 0
+    df = pd.DataFrame()
 
+    def __init__(self, dict, name="", start=-1, end=-1):
+        self.update(dict, name, start, end)
+        
+
+    def update(this, dict, name, start=-1, end=-1):
         timeStamps = list(dict.keys())
         values = list(dict.values())
 
@@ -20,21 +28,10 @@ class Graph:
 
         plt.show(block=False)
 
-
-    def update(this, dict, name, start=-1, end=-1):
-        print()
-        # CONSIDER USING DF.APPEND() from pandas
-
-        # timeStamps = dict.keys()
-        # values = dict.values()
-
-        # df = pd.DataFrame(values, timeStamps, columns=['Time (seconds)'])
-        # df.plot(color = 'red', title = name + ' Over Time')
-
-        # plt.show()
-
-    def delete(self, dict, name, start=-1, end=-1):
-        print()
+    def delete(self, dict, name):
+        df = pd.DataFrame()
+        startIndex = 0
+        endIndex = 0
 
 if __name__ == "__main__": #testing
      testDict = {1: 90, 2: 87, 3: 83, 4: 45, 5: 92, 6: 86, 7: 69, 8: 88, 9: 96, 10: 90, 11: 77}
