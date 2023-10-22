ef0c5f39d3e617ef4324ac9874f240252e62fde4
colbycheese55 <112818133+colbycheese55@users.noreply.github.com>
2023-10-22 15:58:52 -0400

    cleaned up, implimented use of the JimmyParser

--- main.py
+++ main.py
@@ -5,18 +5,16 @@ import test
 import threading
 import serial
 import serial.tools.list_ports
+from messageParser import messageParser
 
 
-running = True
-startTime = None
 
 tractedValues = {"pack_voltage":dict(), "pack_current":dict(), "pack_soc":dict(), "low_cell_voltage":dict(), "high_cell_voltage":dict(), "low_temperature":dict(), "high_temperature":dict(),
                  "battery_voltage":dict(), "battery_current":dict(), "battery_current_direction":dict(), "motor_current":dict(), "fet_temp":dict(), "motor_rpm":dict(), "pwm_duty":dict(), "lead_angle":dict(), "power_mode":dict(), "control_mode":dict(), "accelerator_vr_position":dict(), "regen_vr_position":dict(), "digital_sw_position":dict(), "output_target_value":dict(), "motor_status":dict(), "regen_status":dict(),
-                 "MG0InputCurrent":dict(), "MG0InputVoltage":dict(), "MG1InputCurrent":dict(), "MG1InputVoltage":dict(), "MG0OutputVoltage":dict(), "MG0InputPower":dict(), "MG1OutputVoltage":dict(), "MG1InputPower":dict(), "MG0PCBTemperature":dict(), "MG0MOSFETTemperature":dict(), "MG1PCBTemperature":dict(), "MG1MOSFETTemperature":dict(),
                  "throttle":dict(), "regen":dict(), "cruise_control_speed":dict(), "cruise_control_en":dict(), "forward_en":dict(), "reverse_en":dict(), "motor_on":dict(), "hazards":dict(), "brake_lights":dict(), "headlights":dict(), "left_turn_signal":dict(), "right_turn_signal":dict(), "total_current":dict(), "panel1_voltage":dict(), "panel2_voltage":dict(), "panel3_voltage":dict(), "panel4_voltage":dict(), "panel1_temp":dict(), "panel2_temp":dict(), "panel3_temp":dict(), "panel4_temp":dict()}
 currentGraphs = dict()
 
-def checkForObviousErrors(key, value, message):
+def checkForObviousErrors(key: str, value: int, message: str) -> None:
     maxValues = {"pack_voltage": 150, "pack_current": 110, "low_temperature": 200, "high_temperature": 200}
     minValues = dict()
     if key in maxValues and value > maxValues[key]:
@@ -33,11 +31,8 @@ def checkForObviousErrors(key, value, message):
                 "\nFULL MESSAGE: " + str(message))
     
 
-def messageHandler(unparsedMessage):
-    global startTime
-    if startTime == None:
-        startTime = time.perf_counter()
-    messageDict, timestamp = test.testing() # get from Jimmy's parser
+def messageHandler(unparsedMessage: str) -> None:
+    messageDict, timestamp = messageParser(unparsedMessage)
     if messageDict == None:
         return
     for key in messageDict:
@@ -46,7 +41,7 @@ def messageHandler(unparsedMessage):
         checkForObviousErrors(key, messageDict[key], unparsedMessage)
 
 
-def readInFileData(filePath):
+def readInFileData(filePath: str) -> bool:
     if not os.path.exists(filePath):
         return False
     logData = open(filePath, "r").readlines()
@@ -55,7 +50,7 @@ def readInFileData(filePath):
     print("Import Successful!")
     return True
 
-def findTheSerialPort():
+def findTheSerialPort() -> serial.Serial:
     ports = list(serial.tools.list_ports.comports())
     if len(ports) == 1:
         try:
@@ -68,13 +63,9 @@ def findTheSerialPort():
         print("No ports were found")
     elif len(ports) > 1:
         print("Multiple ports were found, could not determine which is the right one")
+    return None
     
-    time.sleep(2) # retrying
-    print("Trying again...")
-    return findTheSerialPort()
-    
-def serialInputReader(ser):
-    while True:
+def serialInputReader(ser: serial.Serial) -> None:
         try:
             while True:
                 line = ser.readline().decode('utf-8').strip()
@@ -82,11 +73,14 @@ def serialInputReader(ser):
         except KeyboardInterrupt:
             ser.close()
 
-def graphUpdater():
+def graphUpdater() -> None:
     global currentGraphs
-    for graph in currentGraphs.values:
-        graph.update()
-    time.sleep(3)
+    while True:
+        for graph in currentGraphs.values():
+            graph.update()
+            print("here")
+            print (currentGraphs)
+        time.sleep(2)
 
 
 
@@ -97,23 +91,24 @@ if __name__ == "__main__": #Main Method
         inp = input("Enter \"serial\" to read from serial in real time OR enter the path of a log file\n")
         if inp == "serial":
             ser = findTheSerialPort()
+            if ser == None:
+                continue
             serialInputReaderThread = threading.Thread(target=serialInputReader, args=(ser,))
             serialInputReaderThread.start()
-            graphUpdaterThread = threading.Thread(target=graphUpdater)
-            graphUpdaterThread.start()
+            # graphUpdaterThread = threading.Thread(target=graphUpdater)
+            # graphUpdaterThread.start()
             break
         else:
             if readInFileData(inp):
                 break
         print("The entry is not valid, try again")
 
-    while running == True:  # running operation commands
+    while True:  # running operation commands
         command = input("Enter a command: ")
         command = command.split()   # split by spaces; [0] = command, [1] = item name, [2...] = other arguments
 
         if command[0] == "quit":
-            running = False
-            continue
+            break
         if len(command) <= 1:
             print("COMMAND MISSING ARGUMENTS")
             continue
@@ -132,7 +127,7 @@ if __name__ == "__main__": #Main Method
                 del currentGraphs[command[1]]
             case "chg":
                 thisGraph = currentGraphs[command[1]]
-                thisGraph.change(command[2], command[3])
+                thisGraph.changeGraphRange(int(command[2]), int(command[3]))
             case _:
                 print("UNKNOWN COMMAND ENTERED")
 
