f824e7f1c63fd50ac356f44014bca01aa8e2bc61
Jimmy <gbu6ug@virginia.edu>
2023-11-05 16:43:16 -0500

    removed global variables in graphs.py and added instacne variables

--- graphs.py
+++ graphs.py
@@ -11,112 +11,90 @@ import math
 
 class Graph:
     
-    startIndex = 0
-    endIndex = 0
-    start = -1
-    end = -1
-    name = ""
-    logDict = {}
-    df = pd.DataFrame()
-    lastIndex = 0
-    
-
     def __init__(self, inputDict, inputName="", inputStart=-1, inputEnd=-1):
-        global name, logDict, start, end
-        name = inputName
-        logDict = inputDict
-        start = math.ceil(inputStart)
-        end = math.floor(inputEnd)
-        self.update()
+        self.name = inputName
+        self.logDict = inputDict
+        self.start = math.ceil(inputStart)
+        self.end = math.floor(inputEnd)
+        self.lastIndex = 0
+        self.df = pd.DataFrame()
+        self.startIndex = 0
+        self.endIndex = 0
 
     def changeGraphRange(self, newStarting, newEnding):
-        global start, end, startIndex, endIndex
-        
-        start = math.ceil(newStarting)
-        startIndex = start
-        end = math.floor(newEnding)
-        endIndex = end
+        self.start = math.ceil(newStarting)
+        self.startIndex = start
+        self.end = math.floor(newEnding)
+        self.endIndex = end
         self.update()
         
 
     def update(self):
-        
-        global start, end, startIndex, endIndex, name, logDict, df, lastIndex
 
         timeStamps = list(logDict.keys())
         values = list(logDict.values())
-        lastIndex = len(logDict)
-
+        self.lastIndex = len(logDict)
         if(len(timeStamps) < 1):
             print("NO REAL DATA VALUES")
             timeStamps = [0]
             values = [0]
 
-        if(start > -1 and end > -1):
-            if(end > start):
+        if(self.start > -1 and self.end > -1):
+            if(self.end > self.start):
                 print("INPUT ERROR: ending index is smaller than starting index: ")
 
-            print(startIndex, endIndex)
-            startIndex = timeStamps[start]
-            endIndex = timeStamps[end + 1]
+            print(self.startIndex, self.endIndex)
+            self.startIndex = timeStamps[self.start]
+            self.endIndex = timeStamps[self.end + 1]
 
-            df = pd.DataFrame(values[startIndex:endIndex], timeStamps[startIndex:endIndex], columns=['Time (seconds)'])
+            self.df = pd.DataFrame(values[self.startIndex:self.endIndex], timeStamps[self.startIndex:self.endIndex], columns=['Time (seconds)'])
         else:
-            df = pd.DataFrame(values, timeStamps, columns=['Time (seconds)'])
-        df.plot(color = 'red', title = self.name + ' Over Time')
+            self.df = pd.DataFrame(values, timeStamps, columns=['Time (seconds)'])
+        self.df.plot(color = 'red', title = self.name + ' Over Time')
 
         self.ani = FuncAnimation(plt.gcf(), self.update_graph, interval=3000, cache_frame_data=False)
         plt.show(block=False)
 
             
-    def update_animated():
-        global df, logDict, lastIndex
+    def update_animated(self):
 
         print("triggered update_animated()")
-        timeStamps = list(logDict.keys())
-        values = list(logDict.values())
+        timeStamps = list(self.logDict.keys())
+        values = list(self.logDict.values())
 
-        newDF = pd.DataFrame(values[lastIndex:], timeStamps[lastIndex:], columns=['Time (seconds)'])
-        df = pd.concat(df, newDF)
-        lastIndex = len(logDict)
-
-
-
+        newDF = pd.DataFrame(values[self.lastIndex:], timeStamps[self.lastIndex:], columns=['Time (seconds)'])
+        self.df = pd.concat(self.df, newDF)
+        self.lastIndex = len(self.logDict)
 
     def delete(self):
-        global df, startIndex, endIndex
-        df = pd.DataFrame()
-        startIndex = 0
-        endIndex = 0
+        self.df = pd.DataFrame()
+        self.startIndex = 0
+        self.endIndex = 0
         plt.close()
 
     def update_graph(self, i):
-        global data, df, logDict, lastIndex, name
-        
-        timeStamps = list(logDict.keys())
-        values = list(logDict.values())
 
-        newDF = pd.DataFrame(values[lastIndex:], timeStamps[lastIndex:], columns=['Time (seconds)'])
+        timeStamps = list(self.logDict.keys())
+        values = list(self.logDict.values())
+
+        newDF = pd.DataFrame(values[self.lastIndex:], timeStamps[self.lastIndex:], columns=['Time (seconds)'])
         #print(lastIndex, newDF, logDict)
-        lastIndex+=1
-
-
-        data = pd.concat([df, newDF], ignore_index=True)
+        self.lastIndex+=1
+        data = pd.concat([self.df, newDF], ignore_index=True)
 
         
         plt.cla()
         plt.xlabel("Time")
         plt.ylabel("Value")
-        plt.title(name)
+        plt.title(self.name)
         plt.plot(timeStamps, values)
 
     def addNewRow(self, num):
-        global logDict
         while num > 0:
-            logDict[len(logDict)+1] = np.random.randint(0, 100)
+            self.logDict[len(self.logDict)+1] = np.random.randint(0, 100)
             num -= 1
             time.sleep(1)
-            print(logDict)
+            print(self.logDict)
 
 if __name__ == "__main__": #testing
 
