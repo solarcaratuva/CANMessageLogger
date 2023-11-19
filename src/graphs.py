a8ceda025254f192fa646a33a9b46dd43b3f6b0e
Jimmy <gbu6ug@virginia.edu>
2023-11-18 21:15:26 -0500

    fix issue where graph title disappears

--- src/graphs.py
+++ src/graphs.py
@@ -24,10 +24,21 @@ class Graph:
     def update(self, *args):
         timestamps = list(self.dict.keys())
         values = list(self.dict.values())
-        self.ax.clear()  # Clear the current axes
-        self.df = pd.DataFrame({'Values': values}, index=timestamps)
-        self.df.index.name = 'Time (seconds)'
-        self.plot, = self.ax.plot(self.df.index, self.df['Values'])
+
+        if not hasattr(self, 'plot'):
+            # If the plot doesn't exist, create it
+            self.df = pd.DataFrame({'Values': values}, index=timestamps)
+            self.df.index.name = 'Time (seconds)'
+            self.plot, = self.ax.plot(self.df.index, self.df['Values'])
+            plt.title(self.name + " vs Time")
+            plt.xlabel('Time (seconds)')
+            plt.ylabel('Values')
+        else:
+            # If the plot exists, update the data
+            self.plot.set_data(self.df.index, self.df['Values'])
+
+        # Optionally, you can set the title and labels outside the conditional block
+        # to ensure they are always present
         plt.title(self.name + " vs Time")
         plt.xlabel('Time (seconds)')
         plt.ylabel('Values')
