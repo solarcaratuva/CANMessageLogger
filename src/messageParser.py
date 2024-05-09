f0c1b83d451c1cab84be99f2ca8f7b0501082ecd
colbycheese55 <112818133+colbycheese55@users.noreply.github.com>
2024-05-08 21:13:52 -0400

    fixed decode_dbc(), updated messageParser()

--- src/messageParser.py
+++ src/messageParser.py
@@ -1,113 +1,42 @@
 import time
-import os
-import cantools
-from pathlib import Path
-import binascii
-import codecs
+import cantools as ct
+import re
 
-'''
-Cannot find: MG0InputCurrent, MG0InputVoltage, MG1InputCurrent, MG1InputVoltage, MG0OutputVoltage, MG0InputPower
-MG1OutputVoltage, MG1InputPower, MG0PCBTemperature, MG0MOSFETTemperature, MG1PCBTemperature, MG1MOSFETTemperature
-'''
+DBC_FILES = ["BPS.dbc", "MotorController.dbc", "MPPT.dbc", "Rivanna2.dbc"]
+DBS = [ct.db.load_file(f"./src/CAN-messages/{file}") for file in DBC_FILES]
+PATTERN = re.compile(r"^(.*?)\sID\s0x([0-9a-fA-F]+)\sLength\s(\d+)\sData\s0x([0-9a-fA-F]+)$")
+ERRORS_LIST = ['BPSError', 'MotorControllerError', 'PowerAuxError']
+
 timer = None
 
-def decode_dbc(message_id, message_data): #message_id -> frame_id, message_data -> binary representation of the message data
-    # Switch comment below and uncomment line 2 below for testing, vice versa for prod
-    message = make_hex_great_again(message_data)
-    # message = message_data
+def decode_dbc(id_hex: str, data_hex: str) -> dict:
+    id = int(id_hex, 16)
+    data = bytes.fromhex(data_hex.replace("0x", ""))
+    data += b'\x00' * 8 # adding padding
 
-    # message_id = make_hex_great_again(message_id)
-    #print("Message Data: " + str(message_data))
-    #print("Message: " + str(message) + "Message ID: " + str(message_id))
-    curr_path = os.path.dirname(os.path.abspath(__file__))
-    can_dir = os.path.join(curr_path, "CAN-messages")
-
-    bpsDB = cantools.database.load_file(os.path.join(can_dir, "BPS.dbc"))
-    motorControllerDB = cantools.database.load_file(os.path.join(can_dir, "MotorController.dbc"))
-    mpptDB = cantools.database.load_file(os.path.join(can_dir, "MPPT.dbc"))
-    rivanna2DB = cantools.database.load_file(os.path.join(can_dir, "Rivanna2.dbc"))
-
-    #Testing
-    # data = bpsDB.messages
-    #
-    # message_frame_id = 1062
-    # encoded_message = bpsDB.encode_message(message_frame_id, {'low_temperature': 25, 'low_thermistor_id': 1, "high_temperature": 55, "high_thermistor_id": 2})
-    #
-    # message_id = message_frame_id
-    # message_data = encoded_message
-
-    if message_id in bpsDB._frame_id_to_message: # First Returned Value is the Name of the Message, Second Returned Value is a Dictionary with the names and associated values
-        return bpsDB._frame_id_to_message[message_id].name, bpsDB.decode_message(message_id, message)
-    elif message_id in motorControllerDB._frame_id_to_message:
-        return motorControllerDB._frame_id_to_message[message_id].name, motorControllerDB.decode_message(message_id, message)
-    elif message_id in mpptDB._frame_id_to_message:
-        return mpptDB._frame_id_to_message[message_id].name, mpptDB.decode_message(message_id, message)
-    elif message_id in rivanna2DB._frame_id_to_message:
-        return rivanna2DB._frame_id_to_message[message_id].name, rivanna2DB.decode_message(message_id, message)
-    else:
-        return "Error", {"ERROR": "ERROR"}
+    for db in DBS:
+        message_ids = [msg.frame_id for msg in db.messages]
+        if id in message_ids:
+            decoded_message = db.decode_message(id, data)
+            return decoded_message
+    return None
         
-def make_hex_great_again(message_data):
-    ints = []
-    for x in message_data:
-        ints.append(x)
-    new_message = ""
-    for x in ints: #manually convert int to hex
-        if x>=48 and x <= 57:
-            new_message += hex(x-48)[2:]
-        elif x>=65 and x <= 70:
-            new_message += hex(x-55)[2:]
-        else:
-            new_message += hex(0)[2:]
-    if len(new_message) % 2 == 1:
-        new_message = "0" + new_message
 
-    return codecs.decode(new_message, 'hex_codec')
-#only parses msg in the format "messageToFind: key value, ___ _, ___ _, ..."
-def messageParser(message):
-    global timer
+def messageParser(message: str) -> tuple[dict, float]:
     #sets timer to get timestamps
+    global timer
     if timer == None:
         timer = time.perf_counter()
-    messageDict = {}
-    #keywords to look for in the printed log statement
-    messageToFind = ['ECUMotorCommands: ', 'ECUPowerAuxCommands: ', 'BPSPackInformation: ', 'BPSError: ', 'BPSCellVoltage: ', 
-                     'BPSCellTemperature: ', 'MotorControllerFrameRequest: ', 'MotorControllerPowerStatus: ', 
-                     'MotorControllerDriveStatus: ', 'MotorControllerError: ', 'PowerAuxError: ', 'SolarCurrent: ', 
-                     'SolarVoltage: ', 'SolarTemp: ']
-    errorsList = ['BPSError', 'MotorControllerError', 'PowerAuxError']
-    hexMessageToFind = ['ID ', 'Data ']
-    for msg in errorsList:
+    
+    for msg in ERRORS_LIST:
         if msg in message:
             return {"ERROR": "ERROR"}, None
-    split_list = []
-    if hexMessageToFind[0] in message and hexMessageToFind[1] in message:
-        messageIDIndex = message.find(hexMessageToFind[0])
-        messageDataIndex = message.find(hexMessageToFind[1])
-        id_start = messageIDIndex + len(hexMessageToFind[0])
-        id_end = message.find(" ", id_start)
-        can_id = message[id_start:id_end]
-        data_start = messageDataIndex + len(hexMessageToFind[1])
-        can_data = message[data_start:]
-        binaryID = int(can_id, 16)
-        binaryData = bytearray()
-        binaryData.extend(map(ord, can_data))
-        messageName, dataDict = decode_dbc(binaryID, binaryData)
-        return dataDict, time.perf_counter() - timer
-    else:
-        for msg in messageToFind:
-            index = message.find(msg)
-            if msg in message:
-                DataLength = len(msg)
-                break
-        #the key word we are trying to track is not found, returns none
-        if index == -1:
-            return None, time.perf_counter() - timer
-        msgSubstring = message[(index+DataLength):]
-        pair = msgSubstring.split(", ")
-        for string in pair:
-            split_result = string.split()
-            split_list.extend(split_result)
-        for i in range(0, len(split_list), 2):
-            messageDict[split_list[i]] = int(split_list[i+1])
-        return messageDict, time.perf_counter() - timer
+
+    match = re.match(PATTERN, message)
+    if match is None:
+        return None, None
+    canID = match.group(2)
+    canData = match.group(4)
+
+    messageDict = decode_dbc(canID, canData)
+    return messageDict, time.perf_counter() - timer
