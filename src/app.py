4c1377204971e7ae655b548cd7d0c16e74397065
Bryson Matsuda <han3wf@virginia.edu>
2024-10-06 15:57:57 -0400

    set up flask to transfer data from backend to frontend

---
+++ src/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+import backend.db_access as db_access
+
+app = Flask(__name__, template_folder='html')
+
+@app.route('/')
+def index():
+    # Fetch data from the database
+    data = db_access.get_auxiliary_data()
+    # Render the HTML with the large_data passed in
+    return render_template('debug.html', large_data=data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
