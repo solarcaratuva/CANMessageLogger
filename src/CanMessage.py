0e3a26d91d817988784161910200b9785a2207b6
An Huynh <An2huynh@gmail.com>
2024-11-03 15:02:55 -0500

    Fixed up and added DocStrings

--- src/CanMessage.py
+++ src/CanMessage.py
@@ -1,5 +1,4 @@
-import DBCs
-
+from DBCs import DBCs
 
 class CanMessage:
     def __init__(self, name: str, id: int, signals: dict, timestamp: float):
@@ -8,42 +7,35 @@ class CanMessage:
         self.sigDict = signals
         self.timeStamp = timestamp
 
-
-def decode_message(int_id: int, data: bytes, timestamp: float) -> CanMessage:
+@staticmethod
+def decode_message(id: int, data: bytes, timestamp: float) -> CanMessage:
     """
-    Decodes the message using the DBC files.
+    Decodes the message using previously generated DBCs object
 
-    @param int_id: The ID as an int of the message name/type in hexadecimal.
-    @param data: The data as a python byte object of the message in hexadecimal.
+    @param id: The messages's ID as an int
+    @param data: The data as a python byte object
     @param timestamp: timestamp of the message stored as float time from start time.
 
-    @return: A CANmessage object (defined above). Signals are the keys and decoded values are the values. Returns None if the message type is not found in the DBC files.
+    @return: A CANmessage object (defined above). In sigDict, Signals are the keys and decoded values are the values. Returns None if the message type is not found in the DBC files.
     """
     name = None
-    # id = int(id_hex, 16)
+    # id = int(id_hex, 16) Old code, kept for reference
     # data = bytes.fromhex(data_hex.replace("0x", ""))
     data += b'\x00' * 8  # adding padding
 
     decoded_message = None  # dictionary of signals to return
 
-    ''' Colby's old code that did not grab Name
     for db in DBCs:
-        message_ids = [msg.frame_id for msg in db.messages] # problem here is that this just stores .frame_id
-        if id in message_ids: # we need to store the msg object, to get msg.name
-            decoded_message = db.decode_message(id, data)
-    '''
-
-    for db in DBCs.DBCs:
         for msg in db.messages:
-            if msg.frame_id == int_id:
+            if msg.frame_id == id:
                 name = msg.name
-                decoded_message = db.decode_message(int_id, data)
+                decoded_message = db.decode_message(id, data)
                 break
         if decoded_message is not None:
             break
 
-    # decoded message was not associated with a definition from DBCs
+    # if decoded message was not associated with a definition from DBCs, return None
     if decoded_message is None:
         return None
 
-    return CanMessage(name, int_id, decoded_message, timestamp)
+    return CanMessage(name, id, decoded_message, timestamp)
--- src/DbConnection.py
+++ src/DbConnection.py
@@ -1,9 +1,9 @@
 import sqlite3
-import DBCs  # need to have DBCs.py in same directory!! (this is the wrapper file we made for generating DBCs)
+from DBCs import DBCs  # need to have DBCs.py in same directory! (this is the wrapper file we made for generating DBCs)
 import CanMessage  # our own CanMessage Object
 
-# !!!!!!Before initializing any DbConnection, must run setup_the_db_path(path : str)!!!!!!
-DB_path = None  # shared with all DbConnections
+# Before initializing any DbConnection objects, must run setup_the_db_path(path : str)
+DB_path = None  # static, i.e. shared with all DbConnection Objects
 
 
 class DbConnection:
@@ -12,8 +12,19 @@ class DbConnection:
         self.conn.row_factory = sqlite3.Row
         self.cur = self.conn.cursor()
 
-    # helper function that adds a single can_message
+    def __del__(self):
+        self.cur.close()
+        self.conn.close()
+
     def __db_execute(self, can_msg: CanMessage) -> None:
+        """
+        Takes in a CanMessage object, then its signals dictionary are deconstructed and placed into connection's database
+        Does not commit, need to call commit after calling this function
+
+        @param can_msg: A CanMessage object
+
+        @return: None, adds signals from CanMessage object to database. Does not commit. Needs to call commit after.
+        """
         signal_dict = can_msg.sigDict
 
         # Generate the SQL query
@@ -25,6 +36,16 @@ class DbConnection:
 
     @staticmethod
     def __parse_can_message_signals(dbcs: list) -> dict:
+        """
+        Given the DBCs objects in list generated in DBCs.py, functions creates and returns a dictionary containing message
+        type as the keys, and the value being another sub-dictionary that has the signal type as keys and signal data
+        type as values (for now it is always set to datatype of INTEGER).
+
+        @param dbcs: list of DBCs objects to be used to generate dictionary of message types
+
+        @return: a dictionary containing message type as the keys, and the value being another sub-dictionary that has
+        the signal type as keys and signal data type as values (for now it is always set to datatype of INTEGER).
+        """
         can_message_signal_types = {}
         # {'message_name': {signal_name: signal_type, ..., ... }, ..., ...}
 
@@ -36,28 +57,52 @@ class DbConnection:
         return can_message_signal_types
 
     def add_can_msg(self, can_msg: CanMessage) -> None:
-        # need to add code that finds correct table
+        """
+        Add a single Can Message to the connection's database
 
+        @param can_msg: The CanMessage object to be added to database
+
+        @return: None, just adds single CAN message to connection's database
+        """
         self.__db_execute(can_msg)  # helper function defined above
 
         self.conn.commit()
 
     def add_batch_can_msg(self, can_msg_list: list[CanMessage]) -> None:
-        # need to add code that finds correct table
+        """
+        Add a batch (list) of CanMessage objects to the connection's database
 
+        @param can_msg_list: The list of CanMessage objects to be added to database
+
+        @return: None, adds all CanMessage objects to connection's database
+        """
         for can_msg in can_msg_list:
             self.__db_execute(can_msg)  # helper function defined above
 
         self.conn.commit()
 
     def query(self, query: str) -> list[dict]:
+        """
+        Execute a single SQL query and returns what the SQL query returns as a list of dictionaries
+
+        @param query: The query to execute as a String
+
+        @return: list of dictionaries, each dictionary represents a single CANmessage and signals/values as key/values
+        """
         self.cur.execute(query)
         rows = self.cur.fetchall()
         return [dict(row) for row in rows]  # list[ 'can_msg_1' is dict{signal: val,signal: val,signal: val}, ...]
 
     def setup_the_tables(self) -> None:
+        """
+        Sets up the tables in the SQL database, this function must be called before ANY other function calls for the
+        instantiated DbConnection object.
+
+        @return: None, creates a table for each message type (i.e. there will be as many tables as there are
+        message types, as defined in DBCs)
+        """
         # Should only be called once!
-        can_msg_signals = self.__parse_can_message_signals(DBCs.DBCs)
+        can_msg_signals = self.__parse_can_message_signals(DBCs)
 
         # create table for each can_message_name
         for can_msg_type, signal_types_dict in can_msg_signals.items():
@@ -71,5 +116,11 @@ class DbConnection:
 
     @staticmethod
     def setup_the_db_path(path: str) -> None:
+        """
+        This function must be called even before any DbConnection object is instantiated, make sure database file exists
+        and does not have any pre-existing tables.
+
+        @return: None, just sets the SQL database connection path for all DbConnection objects (it is static)
+        """
         global DB_path
         DB_path = path
