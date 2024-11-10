51f45552ede754c3ea538748b44d253c009a9d12
An Huynh <An2huynh@gmail.com>
2024-11-10 15:11:59 -0500

    main.py change to listdir

--- src/DBCs.py
+++ src/DBCs.py
@@ -1,9 +1,10 @@
 # The point of this file is to create DBCs once, instead of repeatedly being recreated whenever DbConnection is called
 import cantools as ct
+import os
 
 # DBC_FILES are the 'definitions'/'mappings' files, they are not parseable yet.
-DBC_FILES = ["BPS.dbc", "MotorController.dbc", "MPPT.dbc", "Rivanna2.dbc"]
-
+dbc_files = os.listdir("./resources/CAN-messages")
+DBCs = [ct.db.load_file(f"./resources/CAN-messages/{file}") for file in dbc_files if file.endswith(".dbc")]
 # DBCs takes the files from DBC_FILES and turns each file into a DBC Object that has functions to access can msg types
 # Function in our code depend on these definitions/configurations to get information on each type of can message.
-DBCs = [ct.db.load_file(f"./resources/{file}") for file in DBC_FILES] # fstring depends on where .dbcs are
+
