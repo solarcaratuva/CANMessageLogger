050d2f894f4e4556999811c81347f250ef7972a2
AbraamIbrahim <abraamibr8@gmail.com>
2025-12-07 19:51:09 -0500

    modified vite config file for socketIO and flask integration

--- src/frontend/race/vite.config.ts
+++ src/frontend/race/vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': 'http://localhost:5000', //where to forward requests with /api/* path
+      '/socket.io': { //where to forward /socket.io/* requests
+        target: 'http://localhost:5000',
+        ws: true, //allow websocket upgrades 
+        changeOrigin: true, //change origin and target headers to match (avoids CORS issues)
+      },
+    },
+  },
+});
+
